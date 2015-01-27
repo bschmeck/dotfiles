d9b254858d3e61d960b9bd97bb843737015a99eb
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-01-26 21:00:39 -0600

    Early work on a bash version of Zach Holman's dotfiles (https://github.com/holman/dotfiles)

---
+++ bash/aliases.bash
@@ -0,0 +1,14 @@
+alias hgcleanup='hg status -un `hg root` | xargs rm -f'
+
+alias edwin='scheme -edwin -edit'
+
+alias log='cd ~/Dropbox/log; emacs postkid05.xml &'
+
+alias appserver='python /home/bschmeck/appengine/dev_appserver.py'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+
